68bf2dddc173074302ad3be3697c11edcbb0a01e
Candra Rahmawan <candraa.rahmawan@gmail.com>
2021-07-08 23:54:45 +0700

    [CR]: update cors middleware specific url to *

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -13,6 +13,6 @@ class CorsMiddleware {
      * @return mixed
      */
     public function handle($request, Closure $next) {
-        return $next($request)->header('Access-Control-Allow-Origin', 'http://localhost:3500,https://maqshaf-web-dev.pesantrenkhoirurrooziqiin.com,https://maqshaf-web.pesantrenkhoirurrooziqiin.com');
+        return $next($request)->header('Access-Control-Allow-Origin', '*');
     }
 }
