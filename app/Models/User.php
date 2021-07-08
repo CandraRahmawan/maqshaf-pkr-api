c2b03469fd628ff97557d85ab4f0fea1ad8de7c2
dickydharmas05 <53419225+dickydharmas05@users.noreply.github.com>
2021-07-08 20:36:12 +0700

    [dicky]: update api upload image

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,26 +8,89 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Support\Facades\DB;
+
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable, HasFactory;
+    
+    protected $connection = 'mysql';
+    public $timestamps = false;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email',
-    ];
+    public static function findAll(){
+        $results = User::select('user_id as userId', 'nis', 'full_name as fullName', 'class', 'address', 'created_at as createdAt', 'created_by as createdBy', 'updated_at as updatedAt', 'updated_by as updatedBy')
+        // ->where('nis', '1111')
+        ->get();
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-    ];
+        return $results;
+    }
+
+    public static function findById($id){
+        $results = User::select('user_id as userId', 'nis', 'full_name as fullName', 'class', 'address', 'created_at as createdAt', 'created_by as createdBy', 'updated_at as updatedAt', 'updated_by as updatedBy')
+        ->where('user_id', $id)
+        ->get();
+
+        return $results;
+    }
+
+    public static function findByIdAndPin($id, $pin){
+        $results = User::select('user_id as userId', 'nis', 'full_name as fullName', 'class', 'address', 'created_at as createdAt', 'created_by as createdBy', 'updated_at as updatedAt', 'updated_by as updatedBy')
+        ->where('user_id', $id)
+        ->where('pin', $pin)
+        ->get();
+
+        return $results;
+
+    }
+
+    public static function insert($data){
+        $user = new User();
+
+        $user->nis = $data['nis'];
+        $user->full_name  = $data['full_name'];
+        $user->class  = $data['class'];
+        $user->address = $data['address'];
+        $user->pin = sha1($data['pin']);
+        $user->created_by = $data['created_by'];
+
+        // User::create(['nis' => 'nis 02']);
+        $user->save();
+        $user->user_id;
+
+        return $user;
+    }
+
+    public static function updateData($id, $data){
+        $user = User::where('user_id', $id)        
+        ->update($data);        
+
+        return $user;
+
+    }
+
+    public static function updatePin($id, $pinOld, $data){
+        DB::beginTransaction();        
+        $results = false;
+
+        try 
+        { 
+            $results = User::where('user_id', $id) 
+            ->where('pin', $pinOld)       
+            ->update($data); 
+            DB::commit();
+            
+        }
+        catch (Exception $e) 
+        {            
+            DB::rollback();
+
+        }        
+
+        return $results;
+
+    }
+
+
+   
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,40 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+
+$router->get('user/all', 'UserController@findAll');
+$router->get('user/{id}', 'UserController@findById');
+$router->get('user/{id}/{pin}', 'UserController@findByIdAndPin');
+$router->post('user/add', 'UserController@insert');
+$router->put('user/update/{id}', 'UserController@updateData');
+$router->put('user/updatepin/{id}', 'UserController@updatePin');
+
+
+$router->get('administrator/all', 'AdministratorController@findAll');
+$router->get('administrator/{id}', 'AdministratorController@findById');
+$router->post('administrator/add', 'AdministratorController@insert');
+$router->post('administrator/login', 'AdministratorController@login');
+$router->put('administrator/update/{id}', 'AdministratorController@updateData');
+$router->put('administrator/updatepassword/{id}', 'AdministratorController@updatePassword');
+$router->delete('administrator/delete/{id}', 'AdministratorController@deleteDataById');
+
+
+$router->get('mastergood/all', 'MasterGoodController@findAll');
+$router->get('mastergood/{id}', 'MasterGoodController@findById');
+$router->post('mastergood/add', 'MasterGoodController@insert');
+$router->put('mastergood/update/{id}', 'MasterGoodController@updateData');
+$router->post('mastergood/upload/image/{id}', 'MasterGoodController@uploadImage');
+
+
+$router->get('deposit/all', 'DepositController@findAll');
+$router->get('deposit/{id}', 'DepositController@findById');
+$router->post('deposit/debet/{userId}', 'DepositController@debit');
+$router->post('deposit/kredit/{userId}', 'DepositController@kredit');
+$router->post('transactions/buy/{userId}', 'DepositController@buyItem');
+
+
+$router->get('transactions/all', 'TransactionsController@findAll');
+$router->get('transactions/{id}', 'TransactionsController@findById');
+$router->post('transactions/add', 'TransactionsController@insert');
+$router->put('transactions/update/{id}', 'TransactionsController@updateData');
