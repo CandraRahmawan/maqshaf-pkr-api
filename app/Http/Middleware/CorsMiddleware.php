8c624fb782a6ebc9b67e09a27f0164a0414ba53b
Candra Rahmawan <candraa.rahmawan@gmail.com>
2021-07-08 23:51:27 +0700

    [CR]: update cors middleware specific url

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -4,23 +4,20 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class CorsMiddleware
-{
+class CorsMiddleware {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
+    public function handle($request, Closure $next) {
         $headers = [
-            'Access-Control-Allow-Origin'      => '*'
+            'Access-Control-Allow-Origin' => 'localhost:3500,maqshaf-web-dev.pesantrenkhoirurrooziqiin.com,maqshaf-web.pesantrenkhoirurrooziqiin.com'
         ];
         $response = $next($request);
-        foreach($headers as $key => $value)
-        {
+        foreach ($headers as $key => $value) {
             $response->header($key, $value);
         }
         return $response;
