b1aec84d801eaa09e507295b6e07648c8f7a8366
Candra Rahmawan <candraa.rahmawan@gmail.com>
2021-08-19 23:28:04 +0700

    chore: update control allow header api_token #6

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -19,7 +19,7 @@ class CorsMiddleware {
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age' => '86400',
             'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With',
-            'Access-Control-Expose-Headers' => '*'
+            'Access-Control-Expose-Headers' => 'Api_Token'
         ];
 
         if ($request->isMethod('OPTIONS')) {
